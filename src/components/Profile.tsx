51ce7109598f61dfa54ecb5c56cbba987b14fc9c
GuilhermeFujita <fujitaguilherme@gmail.com>
2021-02-27 15:00:54 -0300

    Days 01 to 04

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/GuilhermeFujita.png" alt="Guilherme Fujita" />
+      <div>
+        <strong>Guilherme Fujita</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
